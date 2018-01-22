80d187cf35268268a74278d3dfa414bd7f819eb3
somethingwentwrongLOL <35676542+somethingwentwrongLOL@users.noreply.github.com>
2018-01-22 10:29:43 +0530

    Add files via upload

---
+++ first try.py
@@ -0,0 +1,19 @@
+print('blank')
+print('no game no life')
+
+a=11
+b=22
+c=a+b
+print(c)
+
+
+a=input("enter a value")
+b=input("enter b value")
+c=a+b
+print(c)
+
+a=int(input("enter a value"))
+b=int(input("enter b value"))
+c=a+b
+print(c)
+
